115b9413120646fee0c49e1b78e920ce4bdced81
Hussain Bhatia <122187503+Githbhatia@users.noreply.github.com>
2025-05-24 17:55:12 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-
-
-st.logo("HXBLogo.png", size="large")
-main_page= st.Page("Spectra.py", title="ASCE 7-22 Seismic Parameters and Spectra", icon=":material/home:")
-create_page = st.Page("Fpcalc.py", title="Fp Calculations", icon=":material/add_circle:")
-pg = st.navigation([main_page,create_page])
-st.set_page_config(page_title="ASCE722", page_icon=":material/edit:")
-pg.run()
 if 'sds' not in st.session_state:
     st.session_state['sds'] = 0.0
 if 'mytitle' not in st.session_state:
@@ -30,3 +22,11 @@ if "selecteditemTa" not in st.session_state:
     st.session_state.selecteditemTa = 0.0
 if 'checklist_items' not in st.session_state:
     st.session_state.checklist_items = {}
+
+
+st.logo("HXBLogo.png", size="large")
+main_page= st.Page("Spectra.py", title="ASCE 7-22 Seismic Parameters and Spectra", icon=":material/home:")
+create_page = st.Page("Fpcalc.py", title="Fp Calculations", icon=":material/add_circle:")
+pg = st.navigation([main_page,create_page])
+st.set_page_config(page_title="ASCE722", page_icon=":material/edit:")
+pg.run()
